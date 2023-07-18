4c7d790b709e0168e22eae65e35b6cdc54f17931
ahmad-ml <ahmad@niftyitsolution.com>
2023-07-18 10:29:04 +0600

    ✨ feat(App.css): update task list styles to improve readability and add support for completed tasks ✨ feat(App.jsx): add functionality to toggle task completion and update task list rendering to display completed tasks with strikethrough

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ function App() {
   const [newTask, setNewTask] = useState("");
 
   const addTask = (task) => {
-    setTasks([...tasks, task]);
+    setTasks([...tasks, { description: task, completed: false }]);
     setNewTask("");
   }
 
   const removeTask = (taskToRemove) => {
     setTasks(tasks.filter(task => task !== taskToRemove));
+  }
+
+  const toggleTaskCompletion = (taskToToggle) => {
+    setTasks(
+      tasks.map(task =>
+        task === taskToToggle ? { ...task, completed: !task.completed } : task
+      )
+    );
   }
 
   const handleTaskInputChange = (event) => {
@@ -26,20 +34,20 @@ function App() {
     <div>
       <h1>Todo App</h1>
 
-      <form 
+      <form
         onSubmit={event => {
           event.preventDefault();
           addTask(newTask);
         }}
         className="form-container"
       >
-        <input 
+        <input
           value={newTask}
           onChange={handleTaskInputChange}
           placeholder="Enter a new task"
           className="input-field"
         />
-        <button 
+        <button
           type="submit"
           className="submit-btn"
         >
@@ -47,38 +55,52 @@ function App() {
         </button>
       </form>
 
-
       <ul className="task-list">
         {tasks.map((task, index) => (
           <li key={index} className="task-list-item">
-            {task}
-            <button onClick={() => removeTask(task)} className="delete-btn">Delete</button>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTaskCompletion(task)}
+            />
+            <span
+              className={task.completed ? "completed-task" : ""}
+            >
+              {task.description}
+            </span>
+            <button
+              onClick={() => removeTask(task)}
+              className="delete-btn"
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
+
       <footer className="footer">
-      <div className="footer-content">
-        <h3>About {fullName}</h3>
-        <p>
-          Hello, I'm {fullName}, a passionate {occupation}. I love creating innovative and user-friendly web applications that make a difference. Through my work, I strive to push boundaries and explore new possibilities in the digital realm.
-        </p>
-        <p>
-          In my journey as a developer, I've learned that taking risks and embracing failure are key to personal and professional growth. One quote that resonates with me is: 
-        </p>
-        <blockquote className="favorite-quote">
-          "{favoriteQuote}"
-        </blockquote>
-        <p>
-          This quote inspires me to dream big and not be afraid of stumbling along the way. It reminds me that failure is not the end but an opportunity to learn, improve, and achieve greater things.
-        </p>
-      </div>
-      <div className="footer-social">
-        {/* Add your social media links here */}
-      </div>
-      <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} {fullName}. All rights reserved.</p>
-      </div>
-    </footer>
+        <div className="footer-content">
+          <h3>About {fullName}</h3>
+          <p>
+            Hello, I'm {fullName}, a passionate {occupation}. I love creating innovative and user-friendly web applications that make a difference. Through my work, I strive to push boundaries and explore new possibilities in the digital realm.
+          </p>
+          <p>
+            In my journey as a developer, I've learned that taking risks and embracing failure are key to personal and professional growth. One quote that resonates with me is:
+          </p>
+          <blockquote className="favorite-quote">
+            "{favoriteQuote}"
+          </blockquote>
+          <p>
+            This quote inspires me to dream big and not be afraid of stumbling along the way. It reminds me that failure is not the end but an opportunity to learn, improve, and achieve greater things.
+          </p>
+        </div>
+        <div className="footer-social">
+          {/* Add your social media links here */}
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; {new Date().getFullYear()} {fullName}. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 }
